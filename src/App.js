4a7f3ba59c60f0df7087505fe779ad91eb5b8da2
freygold <ahmedtawfik833@gmail.com>
2025-03-26 23:35:37 -0700

    >

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import "./index.css";
+import Main from "./Main";
+function App() {
+   return (
+      <div className="app">
+         <Header />
+         <Main>
+            <p></p>
+            <p></p>
+         </Main>
+      </div>
+   );
+}
+
+export default App;
